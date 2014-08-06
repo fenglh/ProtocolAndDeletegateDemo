187f7ff0b133fd5d97a5e17f72356caca9a92d73
fenglh <335418265@qq.com>
2014-08-06 17:08:23 +0800

    Initial Commit

---
+++ ProtocolDemo/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ProtocolDemo
+//
+//  Created by fenglh on 14-8-6.
+//  Copyright (c) 2014年 yons. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
